f377e80679096a08ac12ceab8cc39596318432d3
Antonio Melendez <antoniomelendez@Antonios-MacBook-Pro.local>
2017-11-07 15:17:13 -0700

    Finished

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base  = Rebase.createClass({
+  apiKey: "AIzaSyCfqi0T4NV7Rwh0YP7uoSJnaSFdQohakjM",
+  authDomain: "catch-of-the-day-melendez.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-melendez.firebaseio.com",
+});
+
+export default base;
